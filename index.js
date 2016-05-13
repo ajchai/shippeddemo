d673a993b573bb59f8c0fde592886c6481776268
Aijun Chai <aijunchai@gmail.com>
2016-05-13 15:48:21 +0800

    added 2016
    added 2016

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello Chai from Cisco Shipped!');
+    res.send('Hello Chai from Cisco Shipped! - 2016');
 });
 
 var server = app.listen(3000, function () {
